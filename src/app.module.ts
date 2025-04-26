8631323572d07496c843d68c18c55e473f01edce
ali asghar sedighian linux co <as.sedighian@gmail.com>
2025-04-26 14:21:08 +0330

    save file in right address on server

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { PrismaModule } from './libs/db/prisma/prisma.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { FileUploadModule } from './modules/files-upload/file-upload.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
     ThrottlerModule.forRoot([{ limit: 15, ttl: 2 * 60 * 1000 }]), //2 minutes
     // Modules
     AuthModule,
