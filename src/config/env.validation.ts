7c62da17ceaf7ef44c206f4f53b22796520eff42
ali asghar sedighian linux co <as.sedighian@gmail.com>
2025-09-17 13:57:37 +0330

    some changes in env

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -2,6 +2,8 @@ import * as Joi from 'joi';
 
 export const validateEnv = (config: Record<string, any>) => {
   const schema = Joi.object({
+    PROTOCOL: Joi.string().default('http://'),
+    DOMAIN: Joi.string().default('localhost:2222'),
     PORT: Joi.number().default(2224),
     DB_NAME: Joi.string().required(),
     DB_HOST: Joi.string().required(),
@@ -10,6 +12,10 @@ export const validateEnv = (config: Record<string, any>) => {
     DB_PASSWORD: Joi.string().required(),
     POSTGRES_DATABASE_URL: Joi.string().required(),
     JWT_SECRET: Joi.string().required(),
+    JWT_REFRESH_SECRET: Joi.string().required(),
+    DOMAIN_ADDRESS: Joi.string().required(),
+    GOOGLE_CLIENT_ID: Joi.string().required(),
+    GOOGLE_CLIENT_SECRET: Joi.string().required(),
   });
 
   const { error, value } = schema.validate(config, {
